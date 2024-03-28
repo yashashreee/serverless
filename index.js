ff782e13fea24d908ca133a8a80eb24c19b9032b
YashashreePatel <yashashreepatel@gmail.com>
2024-03-28 00:24:29 -0400

    updating cloud function

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const { PubSub } = require('@google-cloud/pubsub');
-const { Sequelize, DataTypes } = require('sequelize');
+const { Sequelize } = require('sequelize');
 
 const formData = require('form-data');
 const Mailgun = require('mailgun.js');
@@ -7,14 +6,12 @@ const mailgun = new Mailgun(formData);
 
 require('dotenv').config();
 
-const PROJECT_ID = process.env.PROJECT_ID;
-const PUBSUB_SUBSCRIPTION =  process.env.PUBSUB_SUBSCRIPTION;
-const CLOUDSQL_INSTANCE_CONNECTION_NAME =  process.env.CLOUDSQL_INSTANCE_CONNECTION_NAME;
-const DB_NAME =  process.env.DB_NAME;
-const DB_USER =  process.env.DB_USER;
-const DB_PASS =  process.env.DB_PASS;
-const MAILGUN_API_KEY =  process.env.MAILGUN_API_KEY;
-const MAILGUN_DOMAIN =  process.env.MAILGUN_DOMAIN;
+const CLOUDSQL_INSTANCE_CONNECTION_NAME = process.env.CLOUDSQL_INSTANCE_CONNECTION_NAME;
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const MAILGUN_API_KEY = process.env.MAILGUN_API_KEY;
+const MAILGUN_DOMAIN = process.env.MAILGUN_DOMAIN;
 
 const sequelize = new Sequelize({
   dialect: 'mysql',
@@ -26,68 +23,66 @@ const sequelize = new Sequelize({
   password: DB_PASS
 });
 
-const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY || 'key-yourkeyhere'});
-const pubSubClient = new PubSub({ projectId: PROJECT_ID });
+const mg = mailgun.client({ username: 'api', key: MAILGUN_API_KEY });
 
-async function sendVerificationEmail(event, context) {
+exports.sendVerificationLink = async (event, context) => {
   const pubsubMessage = event.data ? Buffer.from(event.data, 'base64').toString() : '{}';
   const { user, verificationToken } = JSON.parse(pubsubMessage);
 
   try {
-    const expirationTime = Date.now() + 2 * 60 * 1000; // 2 minutes
-    const verificationLink = `https://yashashree.me/verify-email?token=${verificationToken}&expiration=${expirationTime}`;
-    await sendEmail(user.email, verificationLink);
+    const verificationLink = `http://${MAILGUN_DOMAIN}:3000/v1/user/verify-email?token=${verificationToken}&email=${user.email}`;
+    await sendEmail(user, verificationLink);
 
-    await sequelize.query('INSERT INTO TrackEmail (email, verification_token, sent_at) VALUES (?, ?)', {
-      replacements: [user.email, verificationToken, new Date()]
+    await sequelize.query('INSERT INTO TrackEmails (user_id, email, verification_token, sent_at) VALUES (?, ?, ?, ?)', {
+      replacements: [user.id, user.email, verificationToken, new Date()]
+    }).catch(error => {
+      console.error('Error inserting into TrackEmails table:', error);
     });
+
   } catch (error) {
     console.error('Error sending verification email:', error);
   }
 };
 
-async function sendEmail(user, verificationLink) {
+const sendEmail = async (user, verificationLink) => {
   const data = {
     from: 'mailgun@yashashree.me',
     to: user.email,
     subject: 'Verify Your Email Address',
-    text: `<p> Find your details below: ${user} </p> < /br> <p> Click on the following link to verify your email address: ${verificationLink} </p>`
+    html: `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Verify Your Email Address</title>
+      <style>
+        body {
+          font-family: sans-serif;
+          margin: 0;
+          padding: 0;
+        }
+        p {
+          margin: 10px 0;
+        }
+        a {
+          color: #333;
+          text-decoration: none;
+        }
+      </style>
+    </head>
+    <body>
+      <h1>Welcome, ${user.name}!</h1>
+      <p>Thank you for signing up with our service.</p>
+      <p>To complete your registration, please click on the following link to verify your email address:</p>
+      <p><a href="${verificationLink}"> Verify your email here </a></p>
+      <p>Once you verify your email address, you will be able to access other APIs.</p>
+    </body>
+    </html>
+  `
   };
 
-  mg.messages().send(data, (error, body) => {
-    if (error) {
-      console.error('Error sending email:', error);
-    } else {
-      console.log('Email sent:', body);
-    }
-  });
+  mg.messages.create(MAILGUN_DOMAIN, data)
+    .then(msg => console.log("Message sent:", msg)) // Success handler
+    .catch(err => console.error("Error sending message:", err)); // Error handler
 };
-
-
-functions.http('subscribe', (req, res) => {
-  const subscription = pubSubClient.subscription(PUBSUB_SUBSCRIPTION);
-  subscription.on('message', async message => {
-    try {
-      await sendVerificationEmail(message, {});
-      message.ack();
-    } catch (error) {
-      console.error('Error processing message:', error);
-      message.nack();
-    }
-  });
-});
-
-// async function subscribe() {
-//   const subscription = pubSubClient.subscription(PUBSUB_SUBSCRIPTION);
-//   subscription.on('message', async message => {
-//     try {
-//       await exports.sendVerificationEmail(message, {});
-//       message.ack();
-//     } catch (error) {
-//       console.error('Error processing message:', error);
-//       message.nack();
-//     }
-//   });
-// };
-
-// subscribe();
